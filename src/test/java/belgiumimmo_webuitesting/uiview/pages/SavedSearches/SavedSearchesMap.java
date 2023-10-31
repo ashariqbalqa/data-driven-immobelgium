0afe5a5b80ef46fe0eb803f13ef519650330256f
asiq <asharmarch@gmail.com>
2023-10-31 15:51:56 +0500

    Added saved searches page script

--- src/test/java/belgiumimmo_webuitesting/uiview/pages/SavedSearches/SavedSearchesMap.java
+++ src/test/java/belgiumimmo_webuitesting/uiview/pages/SavedSearches/SavedSearchesMap.java
@@ -6,23 +6,55 @@ import org.openqa.selenium.support.FindBy;
 
 public class SavedSearchesMap extends BaseWebUIMap {
     @FindBy (className = "title header-title")
-    private WebElement savedSearchesTitle;
+    private WebElement SavedSearchesTitle;
     @FindBy(xpath = "//div[@class='Dashboard__container dashboard-container pl-4 pr-4 pr-lg-0 py-4 py-lg-0']//div[2]//div[3]//div[1]//button[1]//span[2]")
-    private WebElement editButton;
+    private WebElement EditButton;
     @FindBy(className = "title-modal mt-3")
-    private WebElement editButtonPopUpTitle;
+    private WebElement EditButtonPopUpTitle;
     @FindBy(xpath = "/html[1]/body[1]/div[2]/div[1]/div[2]/div[1]/div[2]/button[1]/span[1]/span[1]/*[name()='svg'][1]/*[name()='path'][1]")
-    private WebElement clickEditSavedSearchCloseButton;
+    private WebElement ClickEditSavedSearchCloseButton;
     @FindBy(xpath="//input[@type='text']")
-    private WebElement updateSavedSearchName;
+    private WebElement UpdateSavedSearchName;
     @FindBy(xpath = "//div[3]/div/div/label/span")
-    private WebElement subscriptionDaily;
+    private WebElement SubscriptionDaily;
     @FindBy(xpath = "(.//*[normalize-space(text()) and normalize-space(.)='Daily'])[1]/following::div[2]")
-    private WebElement subscriptionInstant;
+    private WebElement SubscriptionInstant;
     @FindBy(xpath = "xpath=//div[3]/div/div/label/span")
-    private WebElement subscriptionOff;
+    private WebElement SubscriptionOff;
     @FindBy(xpath = "(.//*[normalize-space(text()) and normalize-space(.)='Delete'])[4]/following::button[1]")
-    private WebElement clickCancelButton;
+    private WebElement ClickCancelButton;
     @FindBy (xpath = "(.//*[normalize-space(text()) and normalize-space(.)='Instant'])[1]/following::span[1]")
-    private WebElement clickDeleteButton;
+    private WebElement ClickDeleteButton;
+
+    public WebElement getSavedSearchesTitle(){
+        return SavedSearchesTitle;
+    }
+    public WebElement getEditButton(){
+        return EditButton;
+    }
+    public WebElement getEditButtonPopUpTitle(){
+        return EditButtonPopUpTitle;
+    }
+    public WebElement getClickEditSavedSearchCloseButton(){
+        return  ClickEditSavedSearchCloseButton;
+    }
+    public WebElement getUpdateSavedSearchName(){
+        return UpdateSavedSearchName;
+    }
+    public WebElement getSubscriptionDaily(){
+        return SubscriptionDaily;
+    }
+    public WebElement getSubscriptionInstant(){
+        return SubscriptionInstant;
+    }
+    public WebElement getSubscriptionOff(){
+        return SubscriptionOff;
+    }
+    public WebElement getClickCancelButton(){
+        return ClickCancelButton;
+    }
+    public WebElement getClickDeleteButton(){
+        return ClickDeleteButton;
+    }
+
 }
--- src/test/java/belgiumimmo_webuitesting/uiview/pages/SavedSearches/SavedSearchesPage.java
+++ src/test/java/belgiumimmo_webuitesting/uiview/pages/SavedSearches/SavedSearchesPage.java
@@ -3,6 +3,8 @@ package belgiumimmo_webuitesting.uiview.pages.SavedSearches;
 import core.base_action.WebAction;
 import core.base_ui.BaseWebUI;
 
+import static org.testng.Assert.assertEquals;
+
 public class SavedSearchesPage extends BaseWebUI<SavedSearchesMap> {
     public SavedSearchesPage(WebAction action) {super(new SavedSearchesMap(),action);
     }
@@ -12,34 +14,68 @@ public class SavedSearchesPage extends BaseWebUI<SavedSearchesMap> {
     }
     public SavedSearchesPage VerifyPageTitle()
     {
+        String ExpectedTitle= "Saved Searches";
+        String ActualTitle;
+
+        ActualTitle= webAction.getText(Map().getSavedSearchesTitle());
+        System.out.println("Actual Title:"+ ActualTitle);
+        assertEquals(ActualTitle, ExpectedTitle);
+
         return this;
     }
     public SavedSearchesPage EditButton()
     {
+        String ExpectedTitle= "Edit Saved Search";
+        String ActualTitle;
+
+        webAction.click(Map().getEditButton(), "EditButton");
+        ActualTitle= webAction.getText(Map().getEditButtonPopUpTitle());
+        System.out.println("Actual Email:"+ ActualTitle);
+        assertEquals(ActualTitle, ExpectedTitle);
         return this;
     }
     public SavedSearchesPage CrossButtonFunctionality()
     {
+        String ExpectedTitle= "Saved Searches";
+        String ActualTitle;
+
+        webAction.click(Map().getClickEditSavedSearchCloseButton(), "EditPop-upCrossButton");
+        ActualTitle= webAction.getText(Map().getSavedSearchesTitle());
+        System.out.println("Actual Email:"+ ActualTitle);
+        assertEquals(ActualTitle, ExpectedTitle);
         return this;
     }
-    public SavedSearchesPage UpdateSavedSearchName()
+    public SavedSearchesPage UpdateSavedSearchName(String savedsearchname)
     {
+        webAction.click(Map().getEditButton(), "EditButton");
+        webAction.type(Map().getUpdateSavedSearchName(), savedsearchname, "SavedSearchName");
         return this;
     }
     public SavedSearchesPage UpdateSubscriptionToDaily()
     {
+        webAction.click(Map().getEditButton(), "EditButton");
+        webAction.scrollIntoView(Map().getSubscriptionDaily());
+
         return this;
     }
     public SavedSearchesPage DisableSubscriptionButton()
     {
+        webAction.click(Map().getEditButton(), "EditButton");
+        webAction.click(Map().getSubscriptionOff(), "Subscriptionoff");
+
         return this;
     }
     public SavedSearchesPage ClickCancelButton()
     {
+        webAction.click(Map().getEditButton(), "EditButton");
+        webAction.click(Map().getClickCancelButton(), "ClickCancelButton");
+
         return this;
     }
     public SavedSearchesPage ClickDeleteButton()
     {
+        webAction.click(Map().getEditButton(), "EditButton");
+        webAction.click(Map().getClickDeleteButton(), "ClickDeleteButton");
         return this;
     }
 
