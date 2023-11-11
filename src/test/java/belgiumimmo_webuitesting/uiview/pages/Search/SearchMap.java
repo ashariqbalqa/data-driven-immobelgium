227923c8e9bda0810c1bee87ec4660e68f8e17d8
asiq <asharmarch@gmail.com>
2023-11-11 22:22:52 +0500

    Added Property Test Cases 1-10

--- src/test/java/belgiumimmo_webuitesting/uiview/pages/Search/SearchMap.java
+++ src/test/java/belgiumimmo_webuitesting/uiview/pages/Search/SearchMap.java
@@ -1,7 +1,72 @@
 package belgiumimmo_webuitesting.uiview.pages.Search;
 
 import core.base_ui.BaseWebUIMap;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class SearchMap extends BaseWebUIMap {
-    
+    @FindBy (name = "search")
+    private WebElement ClickSearchBar;
+    @FindBy (xpath = "//button[@class='filters-icon']")
+    private WebElement ClickFilterButton;
+    @FindBy (css = "//span[@aria-label='close']//*[name()='svg']")
+    private WebElement ClickCrossButtonFilter;
+    @FindBy (xpath="//span[normalize-space()='Show']")
+    private WebElement ClickShowButtonFilter;
+    @FindBy (name = "minPrice")
+    private WebElement EnterMinPriceBox;
+    @FindBy (name = "maxPrice")
+    private WebElement EnterMaxPriceBox;
+    @FindBy (xpath = "//div[@class='ant-modal-body']//div[2]//button[4]")
+    private WebElement Click3NumberOfRooms;
+    @FindBy (xpath = "(.//*[normalize-space(text()) and normalize-space(.)='Any'])[2]/following::span[2]")
+    private WebElement Click2NumberOfBathrooms;//input[@value='A']
+    @FindBy (xpath = "//input[@value='A']")
+    private WebElement SelectEPCLabelA;
+    @FindBy (name = "minSize")
+    private WebElement EnterMinLivingArea;
+    @FindBy (name = "maxSize")
+    private WebElement EnterMaxLivingArea;
+    @FindBy (xpath = "//div[7]//div[2]//div[1]//input[1]")
+    private WebElement EnterMinPlotArea;
+    @FindBy (xpath = "//div[7]//div[2]//div[2]//input[1]")
+    private WebElement EnterMaxPlotArea;
+    @FindBy (xpath = "(.//*[normalize-space(text()) and normalize-space(.)='Property Condition'])[1]/following::span[1]")
+    private WebElement ClickPropertyCondition;
+    public WebElement getClickSearchBar(){
+        return ClickSearchBar;
+    }
+    public WebElement getClickFilterButton(){
+        return ClickFilterButton;
+    }
+    public WebElement getClickCrossButtonFilter(){
+        return ClickCrossButtonFilter;
+    }
+    public WebElement getClickShowButtonFilter(){
+        return ClickShowButtonFilter;
+    }
+    public WebElement getClickMinPriceBox(){
+        return EnterMinPriceBox;
+    }
+    public WebElement getClickMaxPriceBox(){
+        return EnterMaxPriceBox;
+    }
+    public WebElement getClickPropertyCondition(){
+        return ClickPropertyCondition;
+    }
+    public WebElement getClick3NumberOfRooms(){return Click3NumberOfRooms;}
+    public WebElement getClick2NumberOfBathrooms(){return Click2NumberOfBathrooms;}
+    public WebElement getSelectEPCLabelA(){return SelectEPCLabelA;}
+    public WebElement getEnterMinLivingAreaBox(){
+        return EnterMinLivingArea;
+    }
+    public WebElement getEnterMaxLivingArea(){
+        return EnterMaxLivingArea;
+    }
+    public WebElement getEnterMinPlotArea(){
+        return EnterMinPlotArea;
+    }
+    public WebElement getEnterMaxPriceBox(){
+        return EnterMaxPlotArea;
+    }
 }
--- src/test/java/belgiumimmo_webuitesting/uiview/pages/Search/SearchPage.java
+++ src/test/java/belgiumimmo_webuitesting/uiview/pages/Search/SearchPage.java
@@ -1,7 +1,77 @@
 package belgiumimmo_webuitesting.uiview.pages.Search;
 
+import belgiumimmo_webuitesting.uiview.pages.SavedSearches.SavedSearchesPage;
+import core.base_action.WebAction;
 import core.base_ui.BaseWebUI;
 import core.base_ui.BaseWebUIMap;
 
-public class SearchPage extends BaseWebUIMap {
+public class SearchPage extends BaseWebUI<SearchMap> {
+    public SearchPage (WebAction action) {super(new SearchMap(),action);}
+    public SearchPage navigate (String url){
+        webAction.getToUrl("Https://staging.belgiumimmo.be/en/search", true);
+        return this;
+    }
+    public SearchPage SearchBarTest(){
+        webAction.type(Map().getClickSearchBar(), "IMMO NOBELS","SearchBar");
+
+        return this;
+    }
+    public SearchPage ClickFiltersButtonAndCloseButton(){
+        webAction.click(Map().getClickFilterButton(),"Filters Button");
+        webAction.click(Map().getClickCrossButtonFilter(), "Cross Button Filter");
+
+        return this;
+    }
+    public SearchPage ClickFiltersButtonAndShowButton(){
+        webAction.click(Map().getClickFilterButton(),"Filters Button");
+        webAction.click(Map().getClickShowButtonFilter(), "Show Button Filter");
+
+        return this;
+    }
+    public SearchPage EnterValuesInMinMaxBox(){
+        webAction.click(Map().getClickFilterButton(),"Filters Button");
+        webAction.type(Map().getClickMinPriceBox(), "Min Button Box","MinButton");
+        webAction.type(Map().getClickMaxPriceBox(), "Max Button Box","MaxButton");
+
+        return this;
+    }
+    public SearchPage ClickNumberOfRoomsThree(){
+        webAction.click(Map().getClickFilterButton(),"Filters Button");
+        webAction.click(Map().getClick3NumberOfRooms(), "Select Number Of Rooms Three");
+
+        return this;
+    }
+    public SearchPage ClickNumberOfBathrooms2(){
+        webAction.click(Map().getClickFilterButton(),"Filters Button");
+        webAction.click(Map().getClick2NumberOfBathrooms(), "Select Number Of Bathrooms Two");
+
+        return this;
+    }
+    public SearchPage SelectEPCLabelA(){
+        webAction.click(Map().getClickFilterButton(),"Filters Button");
+        webAction.click(Map().getSelectEPCLabelA(), "Select EPC Lable A");
+
+        return this;
+    }
+    public SearchPage EnterValuesInLivingAreaMinMaxBox(){
+        webAction.click(Map().getClickFilterButton(),"Filters Button");
+        webAction.type(Map().getEnterMinLivingAreaBox(), "Min Living Area Box","MinButton");
+        webAction.type(Map().getEnterMaxLivingArea(), "Max Living Area Box","MaxButton");
+
+        return this;
+    }
+    public SearchPage EnterValuesInPlotAreaMinMaxBox(){
+        webAction.click(Map().getClickFilterButton(),"Filters Button");
+        webAction.type(Map().getEnterMinPlotArea(), "Min Plot Area Box","MinButton");
+        webAction.type(Map().getEnterMaxPriceBox(), "Max Plot Area Box","MaxButton");
+
+        return this;
+    }
+    public SearchPage ClickPropertyCondition(){
+        webAction.click(Map().getClickFilterButton(),"Filters Button");
+        webAction.click(Map().getClickPropertyCondition(), "Select Property Condition");
+
+        return this;
+    }
 }
+
